6c3dfa102dcf6a654a878f6787255686bb5f2af7
Andresmejia62 <amejia@uamv.edu.ni>
2025-11-20 18:07:38 -0600

    Se agrega archivo de busqueda lineal

--- CasoEstudio/Program.cs
+++ CasoEstudio/Program.cs
@@ -12,7 +12,37 @@ namespace CasoEstudio
         {
             //Busqueda lineal 
 
-            List
+            List<string> Libros = new List<string>()
+            {
+                "Don Quijote","La Odisea","Divina Comedia", "Rayuela"
+            };
+
+            List<string> Autores = new List<string>()
+            {
+                "Borges","Cortazar","Dante","Homero"
+            };
+
+            Console.WriteLine("=== BÚSQUEDA LINEAL DE LIBROS ===");
+            Console.WriteLine("Ingresa el libro a buscar");
+            string librobuscado = Console.ReadLine();
+
+            bool Encontrado = false;
+
+            for (int i = 0; i < Libros.Count; i++)
+            {
+                if (Libros[i].Equals(librobuscado, StringComparison.OrdinalIgnoreCase))
+                {
+                    Encontrado = true;
+                    // Reemplaza la línea problemática por la siguiente:
+                    Console.WriteLine("Libro encontrado en la posición: " + (i + 1));
+                }
+
+            }
+
+            if (!Encontrado)
+            {
+                Console.WriteLine("El libro no se encuentra en la lista");
+                    }
 
 
         }
